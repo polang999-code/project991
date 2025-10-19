319b4f77f4e3c97d88af94a71e50903e8cc14032
Ali <ali@yahoo.com>
2025-10-19 10:40:32 +0700

    new codes added ti the branch

--- pro999.py
+++ pro999.py
@@ -3,6 +3,11 @@ def square(x):
 
 result = square(10)
 print(result)  # 25
+def area_circle(radius, pi=3.1416):
+    return pi * radius * radius
+
+print(area_circle(3))        # uses default
+print(area_circle(3, 3.14))  # override
 
 
 
